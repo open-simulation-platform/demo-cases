095e835fe3058ff05f2e0e5ff6e28ab7217ddcd0
Thomas Evang <thomas.evang@km.kongsberg.com>
2020-06-09 19:51:24 +0200

    Changed cse to cosim

--- gunnerus-I/start-all.cmd
+++ gunnerus-I/start-all.cmd
@@ -1,7 +1,7 @@
 @echo off
 
-set JAVA_HOME=C:\Program Files\Zulu\zulu-11
-set cseServerGoPath=C:\Users\thomas.evang\Desktop\CSE\server\bin
+set JAVA_HOME=
+set cosimDemoAppPath=
 
 echo Setting JAVA_HOME to %JAVA_HOME%. 
 echo Please alter in script as needed.
@@ -13,19 +13,19 @@ echo terminating running fmu-proxies
 for /f "tokens=1" %%i in ('jps -m ^| find "fmu-proxy"') do ( taskkill /F /PID %%i )
 echo.
 echo terminating running cse-server-go
-for /f "tokens=2" %%i in ('tasklist ^| find "cse-server-go"') do ( taskkill /F /PID %%i )
+for /f "tokens=2" %%i in ('tasklist ^| find "cosim-demo-app"') do ( taskkill /F /PID %%i )
 echo.
 
 echo Starting FMU proxies
 
-start cmd /C ""%JAVA_HOME%"\bin\java -jar "%cseServerGoPath%"\fmu-proxy.jar -thrift/tcp 9090 ./fmus/VesselFmu.fmu"
-start cmd /C ""%JAVA_HOME%"\bin\java -jar "%cseServerGoPath%"\fmu-proxy.jar -thrift/tcp 9091 ./fmus/PMAzimuth.fmu"
-start cmd /C ""%JAVA_HOME%"\bin\java -jar "%cseServerGoPath%"\fmu-proxy.jar -thrift/tcp 9092 ./fmus/PMAzimuth.fmu"
-start cmd /C ""%JAVA_HOME%"\bin\java -jar "%cseServerGoPath%"\fmu-proxy.jar -thrift/tcp 9093"
-start cmd /C ""%JAVA_HOME%"\bin\java -jar "%cseServerGoPath%"\fmu-proxy.jar -thrift/tcp 9094"
+start cmd /C ""%JAVA_HOME%"\bin\java -jar "%cosimDemoAppPath%"\fmu-proxy.jar -thrift/tcp 9090 ./fmus/VesselFmu.fmu"
+start cmd /C ""%JAVA_HOME%"\bin\java -jar "%cosimDemoAppPath%"\fmu-proxy.jar -thrift/tcp 9091 ./fmus/PMAzimuth.fmu"
+start cmd /C ""%JAVA_HOME%"\bin\java -jar "%cosimDemoAppPath%"\fmu-proxy.jar -thrift/tcp 9092 ./fmus/PMAzimuth.fmu"
+start cmd /C ""%JAVA_HOME%"\bin\java -jar "%cosimDemoAppPath%"\fmu-proxy.jar -thrift/tcp 9093"
+start cmd /C ""%JAVA_HOME%"\bin\java -jar "%cosimDemoAppPath%"\fmu-proxy.jar -thrift/tcp 9094"
 
-echo Starting cse-server-go
-start cmd /k ""%cseServerGoPath%"\cse-server-go.exe"
+echo Starting cosim-demo-app
+start cmd /k ""%cosimDemoAppPath%"\cosim-demo-app.exe"
 timeout 2 > nul
 start http://localhost:8000
 
