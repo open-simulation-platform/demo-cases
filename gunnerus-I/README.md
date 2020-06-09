0d103ff0a0103888d285f20271123f0c7737eaba
Thomas Evang <thomas.evang@km.kongsberg.com>
2020-06-09 19:28:59 +0200

    Gunnerus-I demo case

---
+++ gunnerus-I/README.md
@@ -0,0 +1,15 @@
+
+### README
+
+1. Run __start-proxies.bat__ <br>
+    This will start 3 instances of fmu-proxies, which is required for this application to work. 
+    To exit the application enter any key.
+2. Run __run-windows.cmd__. This will start the demo-application server.
+3. Pass the folder containing this file to the demo-application.
+
+
+#### Requirements
+
+* 64-bit Windows
+* A 64-bit JDK ( >= JDK 8) is required in order to run some of the FMUs. 
+There might be issues with JDK 12. JDK 8 is known to work.
---
+++ gunnerus-I/start-all.cmd
@@ -0,0 +1,31 @@
+@echo off
+
+set JAVA_HOME=C:\Program Files\Zulu\zulu-11
+set cseServerGoPath=C:\Users\thomas.evang\Desktop\CSE\server\bin
+
+echo Setting JAVA_HOME to %JAVA_HOME%. 
+echo Please alter in script as needed.
+echo.
+java -version
+echo.
+
+echo terminating running fmu-proxies
+for /f "tokens=1" %%i in ('jps -m ^| find "fmu-proxy"') do ( taskkill /F /PID %%i )
+echo.
+echo terminating running cse-server-go
+for /f "tokens=2" %%i in ('tasklist ^| find "cse-server-go"') do ( taskkill /F /PID %%i )
+echo.
+
+echo Starting FMU proxies
+
+start cmd /C ""%JAVA_HOME%"\bin\java -jar "%cseServerGoPath%"\fmu-proxy.jar -thrift/tcp 9090 ./fmus/VesselFmu.fmu"
+start cmd /C ""%JAVA_HOME%"\bin\java -jar "%cseServerGoPath%"\fmu-proxy.jar -thrift/tcp 9091 ./fmus/PMAzimuth.fmu"
+start cmd /C ""%JAVA_HOME%"\bin\java -jar "%cseServerGoPath%"\fmu-proxy.jar -thrift/tcp 9092 ./fmus/PMAzimuth.fmu"
+start cmd /C ""%JAVA_HOME%"\bin\java -jar "%cseServerGoPath%"\fmu-proxy.jar -thrift/tcp 9093"
+start cmd /C ""%JAVA_HOME%"\bin\java -jar "%cseServerGoPath%"\fmu-proxy.jar -thrift/tcp 9094"
+
+echo Starting cse-server-go
+start cmd /k ""%cseServerGoPath%"\cse-server-go.exe"
+timeout 2 > nul
+start http://localhost:8000
+
